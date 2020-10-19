79ecc570e7b7709a1fe4d62c037b53fc3aa009ab
Alvin <alvinzcoxiv@gmail.com>
2020-10-18 22:25:21 -0400

    assets/images moved to public folder, profile pic now renders as expected

---
+++ src/components/ProfilePic/ProfilePic.js
@@ -0,0 +1,10 @@
+import React from "react"
+// import Images from "../../assets/images"
+
+function ProfilePic(props) {
+    return (
+        <img className="float-md-left mx-auto d-block" src="assets/images/profilepic.jpg" alt="Profile"/>
+    )
+}
+
+export default ProfilePic
--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -3,6 +3,7 @@ import Container from "../components/Container/Container"
 import Row from "../components/Row/Row"
 import Col from "../components/Col/Col"
 import Header from "../components/Header/Header"
+import ProfilePic from "../components/ProfilePic/ProfilePic"
 
 function AboutMe(props) {
     return (
@@ -10,6 +11,11 @@ function AboutMe(props) {
             <Row classes="justify-content-center">
                 <Col classes="contentMain col-md-8 all">
                     <Header header="About Me"></Header>
+                    <Row>
+                        <Col>
+                            <ProfilePic />
+                        </Col>
+                    </Row>
                 </Col>
             </Row>
         </Container>
