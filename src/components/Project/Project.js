2a9bb67fe4ddbb295b1767d86935d1ae028d431d
Alvin <alvinzcoxiv@gmail.com>
2020-10-19 23:55:12 -0400

    made improvements to project component mobile responsiveness, still needs work

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,23 +1,54 @@
-import React from "react"
-import Row from "../Row/Row"
-import Col from "../Col/Col"
-import ColH from "../ColH/ColH"
-import PortHead from "../PortHeader/PortHeader"
-import ProImage from "../ProImage/ProImage"
+import React from "react";
+import Row from "../Row/Row";
+import Col from "../Col/Col";
+import ColH from "../ColH/ColH";
+import PortHead from "../PortHeader/PortHeader";
+import ProImage from "../ProImage/ProImage";
+import ExtLink from "../ExtLink/ExtLink";
 
 function Project(props) {
-    return (
-        <Row classes="justify-content-center my-3 project">
-            <Col classes="col-md-6">
-                <PortHead head={props.head}/>
-                <Row>
-                    <ColH classes="col-6">
-                        <ProImage image={props.image} alt={props.alt}/>
-                    </ColH>
-                </Row>
-            </Col>
+  return (
+    <Row classes="justify-content-center my-3 project">
+      <Col>
+        <PortHead head={props.head} />
+        <Row>
+          <ColH classes="col-6">
+            <Row>
+                <Col classes="d-flex justify-content-center">
+                    <ProImage image={props.image} alt={props.alt} />
+                </Col>
+            </Row>
+            <Row>
+                <Col classes="d-flex justify-content-center">
+                    <ExtLink
+                    to="https://alvinzc4.github.io/Dinner-And-A-Movie/"
+                    text="Procasti-dater"
+                    />
+                </Col>
+            </Row>
+            <Row>
+                <Col classes="d-flex justify-content-center">
+                    <ExtLink
+                    to="https://github.com/AlvinZC4/Dinner-And-A-Movie"
+                    text="Procasti-dater Repo"
+                    />
+                </Col>
+            </Row>
+          </ColH>
+          <ColH classes="col-6 mt-2">
+            <p>
+              Procasti-Dater is an application that allows users to quickly set
+              up a date on the fly. The application will search an area
+              specified by the user for restaurants based on the type of cusine
+              that has been selected and at the same time the app will also show
+              any events that are scheduled in the area (such as concerts or
+              shows).
+            </p>
+          </ColH>
         </Row>
-    )
+      </Col>
+    </Row>
+  );
 }
 
-export default Project
+export default Project;
