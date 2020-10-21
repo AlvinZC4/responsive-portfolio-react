408480122b50066567d8fb760508d389dd87004d
Alvin <alvinzcoxiv@gmail.com>
2020-10-20 23:27:14 -0400

    fixed margin in the project header component

--- src/components/PortHeader/PortHeader.js
+++ src/components/PortHeader/PortHeader.js
@@ -4,7 +4,7 @@ import "./styles.css"
 function PortHeader(props) {
     return (
     <div className="row">
-        <div className="col-12 mb-2 d-flex justify-content-center ml-3 projecthead">
+        <div className="col-12 mb-2 d-flex justify-content-center projecthead">
             <h3>{props.head}</h3>
         </div>
     </div>
