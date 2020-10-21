6d4cd3e4ed0e5be5c8ffd42559d8adc2f9a23561
Alvin <alvinzcoxiv@gmail.com>
2020-10-20 22:09:02 -0400

    modified margins in project component, set props for project component in portfolio.js

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,48 +1,48 @@
 import React from "react";
 import Row from "../Row/Row";
 import Col from "../Col/Col";
-import ColH from "../ColH/ColH";
 import PortHead from "../PortHeader/PortHeader";
 import ProImage from "../ProImage/ProImage";
 import ExtLink from "../ExtLink/ExtLink";
+import "./styles.css"
 
 function Project(props) {
   return (
     <Row classes="justify-content-center my-3 project">
       <Col>
         <PortHead head={props.head} />
-        <Row>
+        <Row classes="mt-2">
           <Col classes="col-lg-6">
             <Row>
-                <Col classes="d-flex justify-content-center">
-                    <ProImage image={props.image} alt={props.alt} />
-                </Col>
+              <Col classes="d-flex mt-3 justify-content-center">
+                <ProImage image={props.image} alt={props.alt} />
+              </Col>
             </Row>
             <Row>
-                <Col classes="d-flex justify-content-center">
-                    <ExtLink
-                    to="https://alvinzc4.github.io/Dinner-And-A-Movie/"
-                    text="Procasti-dater"
-                    />
-                </Col>
+              <Col classes="d-flex justify-content-center">
+                <ExtLink
+                  to={props.toapp}
+                  text={props.textapp}
+                />
+              </Col>
             </Row>
             <Row>
-                <Col classes="d-flex justify-content-center">
-                    <ExtLink
-                    to="https://github.com/AlvinZC4/Dinner-And-A-Movie"
-                    text="Procasti-dater Repo"
-                    />
-                </Col>
+              <Col classes="d-flex justify-content-center">
+                <ExtLink
+                  to={props.torepo}
+                  text={props.textrepo}
+                />
+              </Col>
             </Row>
           </Col>
           <Col classes="col-lg-6 mt-2">
-            <p>
+            <p className="tab">
               Procasti-Dater is an application that allows users to quickly set
-              up a date on the fly. The application will search an area
-              specified by the user for restaurants based on the type of cusine
-              that has been selected and at the same time the app will also show
-              any events that are scheduled in the area (such as concerts or
-              shows).
+              up a date on the fly by searching a specified location for a
+              restaurants based on cusine and events (such as concerts or
+              shows). This app uses Integrated Google Geocode, Google Maps, Google Maps Places, &
+              Ticketmaster API’s and manipulated the DOM with JavaScript by
+              leveraging the DOM API.
             </p>
           </Col>
         </Row>
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,22 +1,30 @@
-import React from "react"
-import Container from "../components/Container/Container"
-import Row from "../components/Row/Row"
-import Col from "../components/Col/Col"
-import Header from "../components/Header/Header"
-import Project from "../components/Project/Project"
+import React from "react";
+import Container from "../components/Container/Container";
+import Row from "../components/Row/Row";
+import Col from "../components/Col/Col";
+import Header from "../components/Header/Header";
+import Project from "../components/Project/Project";
 // import images from "../assets/images"
 
 function Portfolio(props) {
-    return (
-        <Container>
-            <Row classes="justify-content-center">
-                <Col classes="contentMain col-md-8 all">
-                    <Header header="Portfolio"></Header>
-                    <Project head="Procasti-dater" image={require("../assets/images/DinnerDate.png")} alt="Procrasti-Dater"/>
-                </Col>
-            </Row>
-        </Container>
-    )
+  return (
+    <Container>
+      <Row classes="justify-content-center">
+        <Col classes="contentMain col-md-8 all">
+          <Header header="Portfolio"></Header>
+          <Project
+            head="Procasti-dater"
+            image={require("../assets/images/DinnerDate.png")}
+            alt="Procrasti-Dater"
+            toapp={"https://alvinzc4.github.io/Dinner-And-A-Movie/"}
+            textapp={"Procasti-Dater"}
+            torepo={"https://github.com/AlvinZC4/Dinner-And-A-Movie"}
+            textrepo={"Procasti-Dater Repo"}
+          />
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
-export default Portfolio
+export default Portfolio;
